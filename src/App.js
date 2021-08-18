944cb7c3b952d4469ec8aae6c549c67f83fd0957
Joro-Popov <georgi.popov@motion-software.com>
2021-08-18 16:20:50 +0300

    Add Sign-In and Sign-Up pages

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import {Switch, Route} from 'react-router-dom';
 import Header from './components/header';
 import HomePage from "./pages/homepage";
 import ShopPage from './pages/shop';
+import SignIn from './pages/sign-in';
+import SignUp from './pages/sign-up';
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignIn} />
+        <Route path="/signup" component={SignUp} />
       </Switch>
     </div>
   );
