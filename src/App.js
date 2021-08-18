108cd97e65f497bad69dda3cb96b5c11e092ea7e
Joro-Popov <georgi.popov@motion-software.com>
2021-08-18 15:28:15 +0300

    Added header component with options and linking

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import {Switch, Route} from 'react-router-dom';
+import Header from './components/header';
 import HomePage from "./pages/homepage";
 import ShopPage from './pages/shop';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
